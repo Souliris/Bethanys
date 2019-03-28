7c911a6245b2b13ac50fa939947761c3f0ed32f5
James Spencer <jspencer@idealintegrations.net>
2019-03-28 10:37:06 -0400

    Section 1

--- BethanysPieShop/Startup.cs
+++ BethanysPieShop/Startup.cs
@@ -15,6 +15,7 @@ namespace BethanysPieShop
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -22,13 +23,13 @@ namespace BethanysPieShop
         {
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                app.UseDeveloperExceptionPage(); // debug only
+                app.UseStatusCodePages();
+                app.UseStaticFiles();
+                app.UseMvcWithDefaultRoute(); 
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            
         }
     }
 }
