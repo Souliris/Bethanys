a40503ca52f62eef3967481780d3c37ec5046d56
James Spencer <jspencer@idealintegrations.net>
2019-03-28 13:56:22 -0400

    Databse

--- BethanysPieShop/Startup.cs
+++ BethanysPieShop/Startup.cs
@@ -6,6 +6,8 @@ using BethanysPieShop.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace BethanysPieShop
@@ -14,10 +16,21 @@ namespace BethanysPieShop
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
+        public IConfiguration Configuration { get; set; }
+
+        public Startup( IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddTransient<IPieRepository, MockPieRespository>();
-            
+            //services.AddTransient<IPieRepository, MockPieRespository>();
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddTransient<IPieRepository, PieRepositoy>();
+
             services.AddMvc();
         }
 
