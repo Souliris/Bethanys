add7d7cd7a835786246ab2178221184137caa780
James Spencer <jspencer@idealintegrations.net>
2019-03-28 11:00:18 -0400

    Two

---
+++ BethanysPieShop/Models/IPieRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.Models
+{
+    interface IPieRepository
+    {
+        IEnumerable<Pie> GetAppPies();
+        Pie GetPieById(int pieId);
+    }
+}
