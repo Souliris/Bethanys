594da3bf9a499c80fda723dfc7cb22735bea4931
James Spencer <jspencer@idealintegrations.net>
2019-03-28 14:18:02 -0400

    Working site

--- BethanysPieShop/Program.cs
+++ BethanysPieShop/Program.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using BethanysPieShop.Models;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace BethanysPieShop
@@ -14,11 +16,34 @@ namespace BethanysPieShop
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            //CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<AppDbContext>();
+                    DbInitializer.Seed(context);
+
+                }
+                catch (Exception)
+                {
+
+                    // bleh
+                }
+            }
+            host.Run();
+        }
+
+        private static int AppDbContext()
+        {
+            throw new NotImplementedException();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
-    }
-}
+    }// end class
+}//end namespace
