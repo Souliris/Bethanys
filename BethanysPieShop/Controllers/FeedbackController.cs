c7c3a86be92b6d9879caccb8c0112a02ae80fed2
James Spencer <jspencer@idealintegrations.net>
2019-03-29 14:05:17 -0400

    End of course

--- BethanysPieShop/Controllers/FeedbackController.cs
+++ BethanysPieShop/Controllers/FeedbackController.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using BethanysPieShop.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BethanysPieShop.Controllers
 {
+    [Authorize]
     public class FeedbackController : Controller
     {
+        
         private readonly IFeedbackRepository _feedbackRepository;
 
         public FeedbackController(IFeedbackRepository feedbackRepository)
